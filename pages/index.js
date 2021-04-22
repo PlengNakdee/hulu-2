65b510762fd33926e3fd83e7e31053584b0d4e8b
Pleng Nakdee <plengwn@gmail.com>
2021-04-22 22:55:49 +0700

    add header

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Header from "../components/Header";
 
 export default function Home() {
   return (
@@ -7,7 +8,7 @@ export default function Home() {
         <title>Hulu-2.0</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <p>Hello world</p>
+      <Header />
     </div>
   );
 }
