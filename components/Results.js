8af2601be28c56d5a329576aad8d3b391681053c
Pleng Nakdee <plengwn@gmail.com>
2021-04-26 16:29:06 +0700

    add grid

--- components/Results.js
+++ components/Results.js
@@ -2,7 +2,10 @@ import Thumbnail from "./Thumbnail";
 
 function Results({ results }) {
   return (
-    <div>
+    <div
+      className="px-5 my-10 sm:grid md:grid-cols-2 lg:grid-cols-3 
+    xl:grid-cols-4 3xl:flex flex-wrap justify-center"
+    >
       {results.map((result) => (
         <Thumbnail key={result.id} result={result} />
       ))}
--- components/Thumbnail.js
+++ components/Thumbnail.js
@@ -4,7 +4,10 @@ import Image from "next/image";
 function Thumbnail({ result }) {
   const BASE_URL = "https://image.tmdb.org/t/p/original/";
   return (
-    <div className="group">
+    <div
+      className="group cursor-pointer p-2 transition duration-200
+    ease-in transform sm:hover:scale-105 hover:z-50"
+    >
       <Image
         layout="responsive"
         src={
@@ -16,7 +19,12 @@ function Thumbnail({ result }) {
       />
       <div className="p-2">
         <p className="truncate max-w-md">{result.overview}</p>
-        <h2>{result.title || result.original_name}</h2>
+        <h2
+          className="mt-1 text-2xl text-white transition-all
+        duration-100 ease-in-out group-hover:font-bold"
+        >
+          {result.title || result.original_name}
+        </h2>
         <p className="flex items-center opacity-0 group-hover:opacity-100">
           {result.media_type && `${result.media_type} •`}{" "}
           {result.release_date || result.first_air_date} •{" "}
